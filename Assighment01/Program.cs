8f282731b4150c3a82045207da17f861b326ff2a
Lenovo <Lenovo@DESKTOP-4HHMG6Q>
2026-06-09 07:31:55 +0300

    Add project files.

---
+++ Assighment01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Assighment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1: Regions
+            //// ══════════════════════════════════════════════════════════════════════
+            //// QUESTION 2: REGIONS
+            //// ══════════════════════════════════════════════════════════════════════
+            ////
+            //// Q: What is the purpose of #region and #endregion directives in C#? 
+            ////    How do they help in code organization?
+            ////
+            //// ══════════════════════════════════════════════════════════════════════
+
+            ////Nested Region Example
+
+            //Console.WriteLine("\n" + new string('-', 70) + "\n");
+            #endregion
+        }
+
+    } 
+}
