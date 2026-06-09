022ff82ca3af4065180f9c166c7445e954dcb574
ZiadAmr-10 <ziadddddamr@gmail.com>
2026-06-09 14:51:13 +0300

    Question2

--- Assighment01/Program.cs
+++ Assighment01/Program.cs
@@ -12,12 +12,36 @@
             //// Q: What is the purpose of #region and #endregion directives in C#? 
             ////    How do they help in code organization?
             ///   it's used to organize and group code into collapsible sections, making large code files easier to read and navigate.
-                // This helps improve code organization and readability.
+            // This helps improve code organization and readability.
             ////
             // ══════════════════════════════════════════════════════════════════════
             ////Nested Region Example
-           // Console.WriteLine("\n" + new string('-', 70) + "\n");
+            // Console.WriteLine("\n" + new string('-', 70) + "\n");
             #endregion
+            #region Question 2: Variable Declaration - Explicit vs Implicit
+            //// ══════════════════════════════════════════════════════════════════════
+            //// QUESTION 3: VARIABLE DECLARATION - EXPLICIT VS IMPLICIT
+            //// ══════════════════════════════════════════════════════════════════════
+            ////
+            //// Q: What is the difference between explicit and implicit variable 
+            ////    declaration in C#? Provide examples of both.
+            //// Explicit declaration means  the programmer identifies the variable's data type directly 
+            // Explicit declaration
+            //int age = 20;
+            //string name = "Ali";
+            //Implicit declaration  means the compiler automatically identifies the variable's data type using the var 
+            // Implicit declaration
+            //var age = 20;
+            //var name = "Ali";
+            //// ══════════════════════════════════════════════════════════════════════
+
+
+
+
+
+            #endregion
+
+
         }
 
     } 
